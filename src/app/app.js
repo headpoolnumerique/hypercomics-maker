e83854eef27959cb7073d0a8f98bdee17b83c59d
Julien Taquet <julientaq@mac16.local>
2023-04-10 15:53:33 +0200

    fix error on local

--- src/app/app.js
+++ src/app/app.js
@@ -2,17 +2,17 @@ import {
   changeOrientation,
   resizePreview,
   fullPageWatcher,
-} from './modules/preview.js'
+} from "./modules/preview.js";
 import {
   deletePlan,
   deleteAllPlans,
   addPlan,
   duplicatePlan,
-} from './modules/montage.js'
-import { startup } from './modules/startup.js'
-import { uploadToStrapi } from './modules/assetNetwork.js'
-import { deselect, selectLink } from '../app/modules/helpers'
-import { showHideBlock } from './modules/helpers.js'
+} from "./modules/montage.js";
+import { startup } from "./modules/startup.js";
+import { uploadToStrapi } from "./modules/assetNetwork.js";
+import { deselect, selectLink } from "../app/modules/helpers";
+import { showHideBlock } from "./modules/helpers.js";
 import {
   // assetManipulationUi,
   interactObject,
@@ -20,7 +20,7 @@ import {
   moveToLayer,
   updateFromUi,
   updateTheUI,
-} from './modules/assetManipulation.js'
+} from "./modules/assetManipulation.js";
 import {
   previewSpace,
   previewScreen,
@@ -30,210 +30,210 @@ import {
   imageUpload,
   assetsList,
   contextUI,
-} from './modules/selectors.js'
-import { addImg } from './modules/createPreviewElement.js'
-import { connectObjectToPlan } from './modules/dataManagement.js'
-import config from './config/config.js'
+} from "./modules/selectors.js";
+import { addImg } from "./modules/createPreviewElement.js";
+import { connectObjectToPlan } from "./modules/dataManagement.js";
+import config from "./config/config.js";
 
 // list all the things
 //Event and binds
 
-document.querySelectorAll('.previewResizer').forEach((resizeButton) => {
-  resizeButton.addEventListener('click', () => {
+document.querySelectorAll(".previewResizer").forEach((resizeButton) => {
+  resizeButton.addEventListener("click", () => {
     resizePreview(
       previewScreen,
       resizeButton.dataset.previewWidth,
       resizeButton.dataset.previewHeight
-    )
-  })
-})
+    );
+  });
+});
 
-document.querySelector('#orientationChanger').addEventListener('click', () => {
-  changeOrientation(previewScreen)
-})
+document.querySelector("#orientationChanger").addEventListener("click", () => {
+  changeOrientation(previewScreen);
+});
 
-document.querySelector('#fullPageWatcher').addEventListener('click', () => {
-  fullPageWatcher(previewSpace)
-})
+document.querySelector("#fullPageWatcher").addEventListener("click", () => {
+  fullPageWatcher(previewSpace);
+});
 
 //show hide montage
-document.querySelector('#showMontage').addEventListener('click', () => {
-  showHideBlock(montageScreen)
-})
+document.querySelector("#showMontage").addEventListener("click", () => {
+  showHideBlock(montageScreen);
+});
 
 //show hide contextual id
 document
-  .querySelectorAll('#showContextualUI, #closeContextualUI')
+  .querySelectorAll("#showContextualUI, #closeContextualUI")
   .forEach((caller) => {
-    caller.addEventListener('click', () => {
-      showHideBlock(contextUI)
-    })
-  })
+    caller.addEventListener("click", () => {
+      showHideBlock(contextUI);
+    });
+  });
 
-document.querySelector('#addPlan').addEventListener('click', () => {
-  addPlan(montageList, previewScreen)
-})
+document.querySelector("#addPlan").addEventListener("click", () => {
+  addPlan(montageList, previewScreen);
+});
 
-document.querySelector('#deletePlan').addEventListener('click', () => {
-  deletePlan()
-})
+document.querySelector("#deletePlan").addEventListener("click", () => {
+  deletePlan();
+});
 
-document.querySelector('#deleteAllPlans').addEventListener('click', () => {
-  deleteAllPlans()
-})
+document.querySelector("#deleteAllPlans").addEventListener("click", () => {
+  deleteAllPlans();
+});
 
-document.querySelector('#duplicatePlan').addEventListener('click', () => {
-  console.log('click')
+document.querySelector("#duplicatePlan").addEventListener("click", () => {
+  console.log("click");
   const sequenceId = Number(
-    document.querySelector('#sequenceNumber').textContent
-  )
+    document.querySelector("#sequenceNumber").textContent
+  );
   duplicatePlan(
     montageList,
-    document.querySelector('.shown')?.dataset.strapId,
+    document.querySelector(".shown")?.dataset.strapId,
     sequenceId
-  )
-})
-imageUpload.addEventListener('click', function (e) {
-  e.preventDefault()
-  uploadToStrapi(imageUploadInputs)
-})
+  );
+});
+imageUpload.addEventListener("click", function(e) {
+  e.preventDefault();
+  uploadToStrapi(imageUploadInputs);
+});
 
-montageList.addEventListener('click', (e) => {
-  if (e.target.tagName == 'A') {
+montageList.addEventListener("click", (e) => {
+  if (e.target.tagName == "A") {
     //dont change the url of the page because it’s used to define the sequence
-    e.preventDefault()
-    deselect('.selected')
-    selectLink(e.target)
+    e.preventDefault();
+    deselect(".selected");
+    selectLink(e.target);
   }
-})
+});
 
 //when clicking an image in the asset list
-assetsList.addEventListener('click', async (e) => {
+assetsList.addEventListener("click", async (e) => {
   // if it’s an image
-  if (e.target.tagName == 'IMG') {
+  if (e.target.tagName == "IMG") {
     // add the image to the plan
     let planNumber = document
-      .querySelector('.selected')
-      .hash.replace('#plan-', '')
+      .querySelector(".selected")
+      .hash.replace("#plan-", "");
 
-    const assetId = e.target.dataset.assetid
+    const assetId = e.target.dataset.assetid;
 
     let strapisResponse = await connectObjectToPlan(
       config.strapi.url,
       planNumber,
       assetId
-    )
+    );
 
     if (strapisResponse) {
-      console.log(strapisResponse)
+      console.log(strapisResponse);
       addImg(
         e.target,
-        document.querySelector('.selected').hash,
+        document.querySelector(".selected").hash,
         strapisResponse.data.data.id
-      )
+      );
     }
   }
-})
+});
 
 // moveElementOnThePage
 
-preview.addEventListener('click', (event) => {
-  if (event.target.tagName == 'IMG') {
+preview.addEventListener("click", (event) => {
+  if (event.target.tagName == "IMG") {
     previewScreen
-      .querySelector('.asset-selected')
-      ?.classList.remove('asset-selected')
-    event.target.classList.add('asset-selected')
+      .querySelector(".asset-selected")
+      ?.classList.remove("asset-selected");
+    event.target.classList.add("asset-selected");
 
-    updateTheUI(event.target)
+    updateTheUI(event.target);
 
-    interactObject(event.target)
+    interactObject(event.target);
     //update context menu
     // contextUI.querySelector('main').innerHTML = assetManipulationUi
   } else {
     previewScreen
-      .querySelector('.asset-selected')
-      ?.classList.remove('asset-selected')
+      .querySelector(".asset-selected")
+      ?.classList.remove("asset-selected");
 
     //update context menu
     // contextUI.querySelector('main').innerHTML = ''
   }
-})
+});
 
 // contextUI
 
-contextUI.addEventListener('click', function (event) {
-  const asset = previewScreen.querySelector('.asset-selected'),
-    plan = previewScreen.querySelector('.shown')
+contextUI.addEventListener("click", function(event) {
+  const asset = previewScreen.querySelector(".asset-selected"),
+    plan = previewScreen.querySelector(".shown");
 
   switch (event.target.id) {
-    case 'rotate':
-      break
-    case 'resize':
-      break
-    case 'moveFarther':
-      moveToLayer(asset, plan, 'farther')
-      break
-    case 'moveCloser':
-      moveToLayer(asset, plan, 'closer')
-      break
-    case 'moveFarest':
-      moveToLayer(asset, plan, 'farest')
-      break
-    case 'moveClosest':
-      moveToLayer(asset, plan, 'closest')
-      break
+    case "rotate":
+      break;
+    case "resize":
+      break;
+    case "moveFarther":
+      moveToLayer(asset, plan, "farther");
+      break;
+    case "moveCloser":
+      moveToLayer(asset, plan, "closer");
+      break;
+    case "moveFarest":
+      moveToLayer(asset, plan, "farest");
+      break;
+    case "moveClosest":
+      moveToLayer(asset, plan, "closest");
+      break;
     // case 'move':
     //   break
-    case 'deleteAsset':
-      deleteObject()
-      break
+    case "deleteAsset":
+      deleteObject();
+      break;
 
     default:
-      console.log('not there yet')
+      console.log("not there yet");
   }
-})
+});
 
 // show hide/plan
 document
-  .querySelector('#previewPrevious')
-  .addEventListener('click', function () {
-    let shownPlan = document.querySelector('.shown')
-    let shownLink = document.querySelector('.selected')
-    console.log(shownPlan, shownLink)
+  .querySelector("#previewPrevious")
+  .addEventListener("click", function() {
+    let shownPlan = document.querySelector(".shown");
+    let shownLink = document.querySelector(".selected");
+    console.log(shownPlan, shownLink);
     if (!shownPlan.previousElementSibling) {
-      return false
+      return false;
     }
 
-    document.querySelector('.oldshown')?.classList.remove('oldshown')
-    shownPlan.classList.remove('shown')
-    shownLink.classList.remove('selected')
-    shownPlan.previousElementSibling.classList.add('shown')
+    document.querySelector(".oldshown")?.classList.remove("oldshown");
+    shownPlan.classList.remove("shown");
+    shownLink.classList.remove("selected");
+    shownPlan.previousElementSibling.classList.add("shown");
     shownPlan.previousElementSibling?.previousElementSibling?.classList.add(
-      'oldshown'
-    )
+      "oldshown"
+    );
     shownLink
-      .closest('li')
-      .previousElementSibling.querySelector('a')
-      .classList.add('selected')
-  })
+      .closest("li")
+      .previousElementSibling.querySelector("a")
+      .classList.add("selected");
+  });
 
-document.querySelector('#previewNext').addEventListener('click', function () {
-  let shownPlan = document.querySelector('.shown')
-  let shownLink = document.querySelector('.selected')
-  console.log(shownPlan, shownLink)
+document.querySelector("#previewNext").addEventListener("click", function() {
+  let shownPlan = document.querySelector(".shown");
+  let shownLink = document.querySelector(".selected");
+  console.log(shownPlan, shownLink);
   if (!shownPlan.nextElementSibling) {
-    return false
+    return false;
   }
-  shownPlan.classList.remove('shown')
-  shownLink.classList.remove('selected')
-  document.querySelector('.oldshown')?.classList.remove('oldshown')
-  shownPlan.nextElementSibling.classList.add('shown')
-  shownPlan.classList.add('oldshown')
+  shownPlan.classList.remove("shown");
+  shownLink.classList.remove("selected");
+  document.querySelector(".oldshown")?.classList.remove("oldshown");
+  shownPlan.nextElementSibling.classList.add("shown");
+  shownPlan.classList.add("oldshown");
   shownLink
-    .closest('li')
-    .nextElementSibling.querySelector('a')
-    .classList.add('selected')
-})
+    .closest("li")
+    .nextElementSibling.querySelector("a")
+    .classList.add("selected");
+});
 
-startup()
-updateFromUi()
+startup();
+updateFromUi();
--- src/app/modules/startup.js
+++ src/app/modules/startup.js
@@ -1,107 +1,105 @@
-import config from '../config/config.js'
-import { loadSingle, loadCollection, createData } from './dataManagement.js'
-import { addImg, importImg } from './createPreviewElement'
-import { addPlan, renderPlan } from './montage.js'
-import { montageList, sequencePreview } from './selectors.js'
-import { addAssetToTheAssetManager } from './assetManager'
-import { moveToolbars, toggleToolbars } from './toolbarsManipulations.js'
+import config from "../config/config.js";
+import { loadSingle, createData } from "./dataManagement.js";
+import { addPlan, renderPlan } from "./montage.js";
+import { montageList, sequencePreview } from "./selectors.js";
+import { addAssetToTheAssetManager } from "./assetManager";
+import { moveToolbars, toggleToolbars } from "./toolbarsManipulations.js";
 
 async function startup(url = document.location.href) {
   // use parameters to define the url of the project
   // url =  server.com/?sequence=SEQID&project=projectid
-  let sequenceUrl = new URL(url)
-  const projectId = sequenceUrl.searchParams.get('project')
-  const sequenceId = sequenceUrl.searchParams.get('sequence')
+  let sequenceUrl = new URL(url);
+  const projectId = sequenceUrl.searchParams.get("project");
+  const sequenceId = sequenceUrl.searchParams.get("sequence");
 
-  document.body.id = `sequence-${sequenceId}`
-  // sequenceId is enough because it belongs by default to a project
-  // [TODO] link sequence to project
+  document.body.id = `sequence-${sequenceId}`;
 
   // if there is no sequence id, create a new sequence
   if (!sequenceId) {
     let response = await createData(config.strapi.url, `sequences`, {
-      title: 'Title X',
-      projectId: projectId ? projectId : '',
-    })
+      title: "Title X",
+      projectId: projectId ? projectId : "",
+    });
 
     // add the sequence to the URL and show it in the url bar (damn you safari)
-    sequenceUrl.searchParams.set('sequence', response.data.data.id)
+    sequenceUrl.searchParams.set("sequence", response.data.data.id);
 
     // TODO → generate projectId before otherwise, it will create a new page.
 
-    history.pushState({}, null, sequenceUrl)
-    window.location = sequenceUrl
+    history.pushState({}, null, sequenceUrl);
+    window.location = sequenceUrl;
     updateSequenceMeta(
       response.data?.data?.id,
       response.data?.data?.attributes?.title
-    )
+    );
 
-    fillSequence(response.data.data.id)
+    fillSequence(response.data.data.id);
   } else {
     // if there is a sequenceID in the url, load the sequence from strapi
-    let response = await loadSingle(config.strapi.url, `sequences`, sequenceId)
+    let response = await loadSingle(config.strapi.url, `sequences`, sequenceId);
 
     // if the sequence number doesnt exist in strapi, create it
     if (response.data == null) {
       let response = await createData(config.strapi.url, `sequences`, {
-        title: 'this sequence has no name yet',
-        projectId: projectId ? projectId : '',
+        title: "this sequence has no name yet",
+        projectId: projectId ? projectId : "",
         id: sequenceId,
-      })
+      });
 
       //update the sequence url and write in the url bar
-      sequenceUrl.sequenceId = response.data.data.id
+      sequenceUrl.sequenceId = response.data.data.id;
       // history.pushState({}, null, sequenceUrl)
       // window.location.href = sequenceUrl
     }
     updateSequenceMeta(
       response.data?.data?.id,
       response.data?.data?.attributes?.title
-    )
-    fillSequence(response.data.data.id)
+    );
+    fillSequence(response.data.data.id);
   }
-  moveToolbars()
-  toggleToolbars()
+  moveToolbars();
+  toggleToolbars();
 }
 
 async function fillSequence(sequence) {
-  let response = await loadSingle(config.strapi.url, 'sequences', sequence)
-  let plans = response.data.data.attributes.plans
+  let response = await loadSingle(config.strapi.url, "sequences", sequence);
+  let plans = response.data.data.attributes.plans;
   //if there is no plan, create a plan
   if (plans.data.length < 1) {
-    addPlan(montageList, sequence)
+    addPlan(montageList, sequence);
   }
   //create the plan
   plans.data.forEach((plan, index) => {
-    console.log('renderPlan', plan)
+    console.log("renderPlan", plan);
     renderPlan(
       plan,
       montageList,
       sequencePreview,
       index + 1 == plans.data.length ? true : false
-    )
-    fillPlan(plan)
-  })
+    );
+    fillPlan(plan);
+  });
 
   // check for each plan. add them to the view
 }
 
 function updateSequenceMeta(id, title) {
   const meta = {
-    projectName: document.querySelector('#projectName'),
-    sequenceNumber: document.querySelector('#sequenceNumber'),
-  }
-  meta.projectName.innerHTML = title
-  meta.sequenceNumber.innerHTML = id
+    projectName: document.querySelector("#projectName"),
+    sequenceNumber: document.querySelector("#sequenceNumber"),
+  };
+  meta.projectName.innerHTML = title;
+  meta.sequenceNumber.innerHTML = id;
 }
 
 function fillPlan(plan) {
-  console.log('fill the plan on load from the objects')
+  console.log(`fill the plan ${plan.id} on load from the objects`);
+
   // fille the plan with all the existing images
   // find the plan
-  let planToFill = preview.querySelector(`#plan-${plan.id}`)
-  let objectsToFillWith = plan.attributes.objects.data
-  // console.log(objectsToFillWith)
+  let planToFill = preview.querySelector(`#plan-${plan.id}`);
+  let objectsToFillWith = plan.attributes.objects?.data;
+  console.log(plan.attributes, objectsToFillWith);
 
   //
   // // fill the asset manager with the images
@@ -113,24 +111,24 @@ function fillPlan(plan) {
         asset.attributes.location,
         asset.id,
         asset.attributes.filename,
-        document.querySelector('#assetsList')
-      )
+        document.querySelector("#assetsList")
+      );
       planToFill.insertAdjacentHTML(
-        'beforeend',
+        "beforeend",
         `<img id="inuse-${plan.id}-${object.id}" data-objectId="${
           object.id
         }" data-planid="${plan.id}"
         data-assetid="${asset.id}" src="${
           asset.attributes.location
         }" class="asset" style="${
-          object.attributes.width ? `width:${object.attributes.width}` : ''
+          object.attributes.width ? `width:${object.attributes.width}` : ""
         }
-        ${object.attributes.height ? `height:${object.attributes.height}` : ''}
-        ${object.attributes.top ? `top:${object.attributes.top}` : ''}
-        ${object.attributes.left ? `left:${object.attributes.left}` : ''}" >`
-      )
-    })
-  })
+        ${object.attributes.height ? `height:${object.attributes.height}` : ""}
+        ${object.attributes.top ? `top:${object.attributes.top}` : ""}
+        ${object.attributes.left ? `left:${object.attributes.left}` : ""}" >`
+      );
+    });
+  });
 }
 
-export { startup, fillPlan }
+export { startup, fillPlan };
