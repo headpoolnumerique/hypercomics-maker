f34471e3ace90df2160d287898a35314b3cf9e37
Julien Taquet <julientaq@mac16.local>
2024-09-05 22:50:32 +0200

    add site url in config

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,7 +1,9 @@
 const config = {
-  strapi : {
+  appurl: "https://localhost:8080",
+  strapi: {
     // url : 'https://strapi.lesgoujons.fr',
-    url : 'http://localhost:1337',
-}}
+    url: "http://localhost:1337",
+  },
+};
 
-export default config 
+export default config;
